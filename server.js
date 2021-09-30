5475392bd070ae140e8f2dbcad44a9bb76ad39ec
Alba Molina <89818140+albavmolina@users.noreply.github.com>
2021-09-29 20:27:08 -0400

    Add files via upload
    "added foods"

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 // require database and set up dependencies 
 const drinks = require('./models/drinks.js')
+const foods = require('./models/foods.js')
 
 // include a get route / that will res.send('Welcome to the Gitpub App!');so that when you got to localhost:3000, you will see Welcome to the Gitpub App!
 
@@ -25,7 +26,23 @@ app.get('/drinks/:id', (req, res) => {
         drink:drinks[req.params.id]
         });
         });
+        
+app.get('/', (req, res) => {
+res.send(' Welcome to the Gitpub App!');
+})
 
+app.get("/foods/", (req, res) => {
+        res.render("foods_index.ejs", {
+        food: foods,
+        });
+        });
+
+app.get('/foodss/:id', (req, res) => {
+        foods[req.params.id];
+        res.render('foods_show.ejs', {
+        food:foods[req.params.id]
+        });
+        });
 
 // set a variable of port to 3000
 const port = 3000;
